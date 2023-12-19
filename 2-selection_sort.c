40babca7c93f0faf1f97c4bf0c6d3f48fde25385
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-12-19 09:40:19 -0800

    ..

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,6 +10,7 @@
 void selection_sort(int *array, size_t size)
 {
 size_t i, j, min_idx;
+int temp;
 for (i = 0; i < size - 1; i++)
 {
 min_idx = i;
@@ -20,7 +21,7 @@ min_idx = j;
 }
 if (min_idx != i)
 {
-int temp = array[i];
+temp = array[i];
 array[i] = array[min_idx];
 array[min_idx] = temp;
 print_array(array, size);
